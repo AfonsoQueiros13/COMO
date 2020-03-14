61fc4a62cd8479af33b95cfa316ad64f676fcc4a
Jorge Afonso Barandas Queirós <afonsoqueiros13@gmail.com>
2020-03-14 12:25:56 +0000

    Update reader.py

--- code/reader.py
+++ code/reader.py
@@ -31,8 +31,31 @@ n, bins, patches = plt.hist(values, bins=bins_values, facecolor='g', alpha=0.75,
 plt.show()
 x = [0,1,2,3,4,5,6,7]
 
+x = [0,1,2,3,4,5,6,7]
 plt.bar(x,n/len(values))
+plt.title("Percentage of area per RX dbm")
+plt.xticks(ticks=x,labels=["<0","0-10","10-20","20-30","30-40","40-50","50-60",">60"])
 plt.show()
+
+acc = 0
+
+cum = [0]*len(n)
+print(cum)
+for i in range(len(n)):
+    acc+=n[i]
+    cum[i] = acc/len(values)
+
+
+
+plt.bar(x,cum)
+plt.title("prob cumulative")
+plt.xticks(ticks=x,labels=["<0","0-10","10-20","20-30","30-40","40-50","50-60",">60"])
+plt.show()
+
+
+print(n[0]/len(values))
+print((n[0]+n[1])/len(values))
+
 
 
 
